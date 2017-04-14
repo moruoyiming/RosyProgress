4eb73b7759a4559849da36b1335d1ffccfefa4a0
jianruilin <798774875@qq.com>
2017-04-14 17:03:36 +0800

    Initial commit

---
+++ rosyprogresslib/src/main/java/com/calypso/rosyprogress/ProgressConfig.java
@@ -0,0 +1,22 @@
+package com.calypso.rosyprogress;
+
+/**
+ * Project：RosyProgress
+ * Created：jianz
+ * Date：2017/4/14 15:49
+ * Summry：
+ */
+
+public class ProgressConfig {
+    public static final boolean ISSHOW_SMALL = true;
+    public static final int DEFAULT_MAX_VALUE = 100;
+    public static final int DEFAULT_VALUE = 50;
+    public static final int CIRCLE_STROKE_WIDTH = 6;
+    public static final int CIRCLE_RADIUS = 100;
+    public static final int SMALL_CIRCLE_STROKE_WIDTH = 2;
+    public static final int SMALL_CIRCLE_RADIUS = 6;
+    public static final int TEXT_SIZE = 40;
+    public static final int PROGRESS_WIDTH = 6;;
+
+
+}
